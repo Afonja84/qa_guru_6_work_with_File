da07acdfd13b9c23ef55e17f213c91fbeada1174
usatenko <usatenko_e@mail.ru>
2021-08-08 13:33:12 +0300

    + test PDF

--- src/test/java/qa_guru_workWithFile/PdfTest.java
+++ src/test/java/qa_guru_workWithFile/PdfTest.java
@@ -7,17 +7,16 @@ import org.junit.jupiter.api.Test;
 import java.io.File;
 import java.io.IOException;
 
-import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class PdfDownloadTest {
+public class PdfTest {
 
     @Test
     void junitPdfTest() throws IOException {
-        Selenide.open("https://junit.org/junit5/docs/current/user-guide/");
-        File pdfDownloadedFile = $(byText("PDF download")).download();
+        Selenide.open("https://github.com/Afonja84/qa_guru_7_work_with_File/blob/master/src/test/resources/resume.pdf");
+        File pdfDownloadedFile = $("#raw-url").download();
         PDF parsedPdf = new PDF(pdfDownloadedFile);
-        assertTrue(parsedPdf.author.contains("Marc Philipp"));
+        assertTrue(parsedPdf.text.contains("Усатенко"));
     }
 }
