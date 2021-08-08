48f129440d533053325dc072a1955ed1af45e6a6
usatenko <usatenko_e@mail.ru>
2021-08-08 14:32:24 +0300

    + test zip

--- src/test/java/qa_guru_workWithFile/ArhivTest.java
+++ src/test/java/qa_guru_workWithFile/ArhivTest.java
@@ -1,33 +1,56 @@
-//package qa_guru_workWithFile;
-//
-//import org.apache.commons.io.IOUtils;
-//import org.junit.jupiter.api.Test;
-//
-//import java.io.InputStream;
-//import java.nio.charset.StandardCharsets;
-//import java.util.zip.ZipInputStream;
-//
-//import static com.codeborne.pdftest.assertj.Assertions.assertThat;
-//
-//
-//public class ArhivTest {
-//
-//    static final String  PASSWORD = "123";
-//    static final String UNZIP_FILE_PATH = "unzip";
-//    static final String UNZIPPED_FILE_PATH = "./unzip/TestTxtFile.txt";
-//
-//    @Test
-//    public void zipResourcesTest() throws Exception {
-//        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-//        InputStream stream = classLoader.getResourceAsStream("arhivetest.zip");
-//        String entryAsString = null;
-//        assert stream != null;
-//        try (ZipInputStream zipInputStream = new ZipInputStream(stream, StandardCharsets.UTF_8)) {
-//            while ((zipInputStream.getNextEntry()) != null) {
-//                entryAsString = IOUtils.toString(zipInputStream, StandardCharsets.UTF_8);
-//            }
-//        }
-//        assertThat(entryAsString).contains("Hello World");
-//    }
-//
-//}
+package qa_guru_workWithFile;
+
+import net.lingala.zip4j.ZipFile;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.zip.ZipInputStream;
+
+public class ArhivTest {
+
+    private static final String ZIP_FILE = "./src/test/resources/arhiveWithPassword.zip";
+    private static final String DOC_FILE = "./src/test/resources/unzip.doc";
+    private static final String PASSWORD = "123";
+
+    @Test
+    public void arhiveWithoutPasswordTest() throws Exception {
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        InputStream stream = classLoader.getResourceAsStream("arhivetest.zip");
+        String entryAsString = null;
+        assert stream != null;
+        try (ZipInputStream zipInputStream = new ZipInputStream(stream, StandardCharsets.UTF_8)) {
+            while ((zipInputStream.getNextEntry()) != null) {
+                entryAsString = IOUtils.toString(zipInputStream, StandardCharsets.UTF_8);
+            }
+        }
+        Assertions.assertTrue(entryAsString.contains("Hello World"));
+    }
+
+    @Test
+    void arhiveWithPasswordTest() throws IOException {
+        String zipFilePath = "./src/test/resources/arhiveWithPassword.zip";
+        String unzipFolderPath = "./src/test/resources";
+        String zipPassword = "123";
+        String unzipTxtFilePath = "./src/test/resources/unzip_txt.txt";
+
+        ZipFile zipFile = new ZipFile(zipFilePath);
+        if (zipFile.isEncrypted()) {
+            zipFile.setPassword(zipPassword.toCharArray());
+        }
+        zipFile.extractAll(unzipFolderPath);
+
+        String fileContent = FileUtils.readFileToString(FileUtils
+                .getFile(unzipTxtFilePath), "UTF-8");
+
+        Assertions.assertTrue(fileContent.contains("Hello"));
+    }
+}
+
+
+
