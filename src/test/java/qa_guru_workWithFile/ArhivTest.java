20bea92a06ee0e9082721550ce68e20a159c3247
usatenko <usatenko_e@mail.ru>
2021-08-08 12:37:47 +0300

    + арзив без пароля

---
+++ src/test/java/qa_guru_workWithFile/ArhivTest.java
@@ -0,0 +1,32 @@
+package qa_guru_workWithFile;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.jupiter.api.Test;
+
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.zip.ZipInputStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ArhivTest {
+
+    static final String  PASSWORD = "123";
+    static final String UNZIP_FILE_PATH = "unzip";
+    static final String UNZIPPED_FILE_PATH = "./unzip/TestTxtFile.txt";
+
+    @Test
+    public void zipResourcesTest() throws Exception {
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        InputStream stream = classLoader.getResourceAsStream("arhivetest.zip");
+        String entryAsString = null;
+        assert stream != null;
+        try (ZipInputStream zipInputStream = new ZipInputStream(stream, StandardCharsets.UTF_8)) {
+            while ((zipInputStream.getNextEntry()) != null) {
+                entryAsString = IOUtils.toString(zipInputStream, StandardCharsets.UTF_8);
+            }
+        }
+        assertThat(entryAsString).contains("Hello World");
+    }
+
+}
